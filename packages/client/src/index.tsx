396c86139a8b5d2fe112eca7cd65ffbafbf19f0f
Luca Perotti <lperotti@ranorex.com>
2020-07-16 22:56:22 +0200

    chore(all): remove comments

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -5,23 +5,6 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
 
 const cache = new InMemoryCache(
-  // {
-  //   typePolicies: {
-  //     PokemonsConnection: {
-  //       fields: {
-  //         edges: {
-  //           // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //           merge(existing: any[], incoming: any[]) {
-  //             console.log("fooooooooooooo", existing, incoming)
-  //             return [...(existing || []), ...incoming];
-  //           },
-  //         }
-  //       }
-  //     }
-  //   }
-
-
-  // }
 );
 
 const client = new ApolloClient({
